1ed478232dd3b3a125c3e6c6c35c99abd018d548
Mervyn Lee <Mervyn Lee>
2017-05-07 13:17:11 +0800

    3

--- web.js
+++ web.js
@@ -1,5 +1,14 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
  
 app.use(express.static(__dirname + "/www"));
 app.listen(process.env.PORT || 5000);
+*/
+
+var express = require('express'),
+app = express();
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
